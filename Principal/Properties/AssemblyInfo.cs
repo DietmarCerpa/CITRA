a1681b504add475e21f444b1915f4a3fafc507d4
Edmara Guerrero <edxil_coil@hotmail.com>
2018-12-17 01:27:14 -0400

    Colocar logo a la aplicacion

--- Principal/Properties/AssemblyInfo.cs
+++ Principal/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // La información general sobre un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos atributos para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("Principal")]
+[assembly: AssemblyTitle("CITRA")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Luca Guerrero")]
 [assembly: AssemblyProduct("Principal")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
